e26b6823ec7923b009d632465a97449611668154
LuisOlivo340 <39189778+LuisOlivo340@users.noreply.github.com>
2020-06-05 17:53:39 -0500

    Agregar el servidor

---
+++ Servidor/ThriftCalc.thrift
@@ -0,0 +1,6 @@
+service CalculadoraBasica {
+    i32 Sumar( 1: i32 a, 2: i32  b)
+    i32 Restar( 1: i32 a, 2: i32  b )
+    i32 Multiplicar( 1: i32 a, 2: i32  b )
+    i32 Dividir( 1: i32 a, 2: i32  b )
+}
